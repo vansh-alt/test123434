348d13b54342cdbc3b0799e1ff804aefbebfc7a5
Vansh Soni <vanshsoni16@gmail.com>
2026-09-17 16:54:57 +0530

    SR-12762 - Solved issue

--- testcase2.js
+++ testcase2.js
@@ -1,6 +1,7 @@
 const { exec } = require("child_process");
 
 const variable = "test";
+let count = 0;
 
 function pingHost(userInputHost, callback) {
   const command = `ping -c 1 ${userInputHost}`;
@@ -9,8 +10,9 @@ function pingHost(userInputHost, callback) {
     if (error) {
       return callback(error, null);
     }
+    count++;
     return callback(null, stdout);
   });
 }
 
-module.exports = { pingHost };
+module.exports = { pingHost, count };
